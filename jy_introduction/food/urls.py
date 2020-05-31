8ba2e17ce4a43a4869fecfd39c78da505790c269
2JooYeon <lewndus7749@naver.com>
2020-05-31 22:04:06 +0900

    hackathon last

--- jy_introduction/food/urls.py
+++ jy_introduction/food/urls.py
@@ -5,10 +5,10 @@ import food.views
 
 urlpatterns = [
     path('food/',food.views.food, name= "food"),
-    path('food/<int:food_id>', food.views.detail, name = "detail"),
-    path('food/new',food.views.new, name = "new"),
-    path('food/create',food.views.create, name = "create"),
-    path('food/edit/<int:food_id>', food.views.edit, name = "edit"),
-    path('food/update/<int:food_id>', food.views.update, name = "update"),
-    path('food/delete/<int:food_id>', food.views.delete, name = "delete"),
+    path('food/<int:food_id>', food.views.detail, name = "food_detail"),
+    path('food/new',food.views.new, name = "food_new"),
+    path('food/create',food.views.create, name = "food_create"),
+    path('food/edit/<int:food_id>', food.views.edit, name = "food_edit"),
+    path('food/update/<int:food_id>', food.views.update, name = "food_update"),
+    path('food/delete/<int:food_id>', food.views.delete, name = "food_delete"),
 ]
--- jy_introduction/travel/urls.py
+++ jy_introduction/travel/urls.py
@@ -5,10 +5,10 @@ import travel.views
 
 urlpatterns = [
     path('travel/',travel.views.travel, name= "travel"),
-    path('travel/<int:travel_id>', travel.views.detail, name = "detail"),
-    path('travel/new',travel.views.new, name = "new"),
-    path('travel/create',travel.views.create, name = "create"),
-    path('travel/edit/<int:travel_id>', travel.views.edit, name = "edit"),
-    path('travel/update/<int:travel_id>', travel.views.update, name = "update"),
-    path('travel/delete/<int:travel_id>', travel.views.delete, name = "delete"),
+    path('travel/<int:travel_id>', travel.views.detail, name = "travel_detail"),
+    path('travel/new',travel.views.new, name = "travek_new"),
+    path('travel/create',travel.views.create, name = "travel_create"),
+    path('travel/edit/<int:travel_id>', travel.views.edit, name = "travel_edit"),
+    path('travel/update/<int:travel_id>', travel.views.update, name = "travel_update"),
+    path('travel/delete/<int:travel_id>', travel.views.delete, name = "travel_delete"),
 ]
